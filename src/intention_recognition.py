9981e08db92f5f97f10cd63da02e593833690d75
Sera BUYUKGOZ <sera.buyukgoz@softbankrobotics.com>
2021-07-09 06:22:04 +0200

    result of intent parathesis typo fixed

--- src/intention_recognition.py
+++ src/intention_recognition.py
@@ -8,7 +8,7 @@ class Intention():
     def __init__(self):
         self.plan_map = {}
         self.history_of_intention = {} #according to time
-        self.history_of_desirability = {}
+        #self.history_of_desirability = {}
 
     def create_recogniser(self, list_of_goals, domain, problem):
     #Create map of all plan and goal
@@ -46,57 +46,63 @@ class Intention():
 
         #Added intetntion list to the list of history_of_intention according to time
         self.history_of_intention[time.time()] = intent_list.copy()
+        intent_map = {}
+        for i in intent_list:
+            intent_map[i] = []
+            for list_element in maped[i]:
+                temp = each_action = "(" + each_action  + ")"
+                intent_map[i].append(temp)
 
-        return intent_list
+        return intent_list, intent_map
 
-    def return_desirability_value(self):
-        els = list(self.history_of_desirability)
-        return self.history_of_desirability[els[-1]]
-
-    def desirability_detection(self, intent_list, len_list_goal):
-        '''
-            Function of desirability detection;
-            It depends on frequent was intended action on the plan_list
-        '''
-        map_desireable = {}
-
-        #chose the last one
-
-
-        #update value according to frequency of appear
-        for item in intent_list:
-            values = self.bayesian_probability(item, len(intent_list),len_list_goal)
-            map_desireable[item] = values
-
-        map = self.normalise_value(map_desireable)
-
-        self.history_of_desirability[time.time()] = map
-        return map
-
-    def normalise_value(self, maped_temp):
-        total_count = 0
-
-        for each in maped_temp:
-            total_count += maped_temp[each]
-
-        for each in maped_temp:
-            maped_temp[each] = maped_temp[each] / total_count
-
-        return maped_temp
-
-    def bayesian_probability(self, intended_action, len_intended_action, len_list_goal):
-
-        count = 0
-        length = 0
-        for e in self.history_of_intention:
-            length += 1
-            if intended_action in self.history_of_intention[e]:
-                count += 1
-
-
-        frequency_occurance = count / length
-        current_prob = 1 / len_intended_action
-        general_prob = 1 / len_list_goal
-
-        value = (frequency_occurance + current_prob) / general_prob
-        return value
+    # def return_desirability_value(self):
+    #     els = list(self.history_of_desirability)
+    #     return self.history_of_desirability[els[-1]]
+    #
+    # def desirability_detection(self, intent_list, len_list_goal):
+    #     '''
+    #         Function of desirability detection;
+    #         It depends on frequent was intended action on the plan_list
+    #     '''
+    #     map_desireable = {}
+    #
+    #     #chose the last one
+    #
+    #
+    #     #update value according to frequency of appear
+    #     for item in intent_list:
+    #         values = self.bayesian_probability(item, len(intent_list),len_list_goal)
+    #         map_desireable[item] = values
+    #
+    #     map = self.normalise_value(map_desireable)
+    #
+    #     self.history_of_desirability[time.time()] = map
+    #     return map
+    #
+    # def normalise_value(self, maped_temp):
+    #     total_count = 0
+    #
+    #     for each in maped_temp:
+    #         total_count += maped_temp[each]
+    #
+    #     for each in maped_temp:
+    #         maped_temp[each] = maped_temp[each] / total_count
+    #
+    #     return maped_temp
+    #
+    # def bayesian_probability(self, intended_action, len_intended_action, len_list_goal):
+    #
+    #     count = 0
+    #     length = 0
+    #     for e in self.history_of_intention:
+    #         length += 1
+    #         if intended_action in self.history_of_intention[e]:
+    #             count += 1
+    #
+    #
+    #     frequency_occurance = count / length
+    #     current_prob = 1 / len_intended_action
+    #     general_prob = 1 / len_list_goal
+    #
+    #     value = (frequency_occurance + current_prob) / general_prob
+    #     return value
