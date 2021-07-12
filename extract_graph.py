4b533a1f5dc7424871e67a2ab59d032aa1d5a1f3
Sera Buyukgoz <sera.buyukgoz@softbankrobotics.com>
2021-07-12 19:00:01 +0200

    clear scenario

--- extract_graph.py
+++ extract_graph.py
@@ -31,16 +31,34 @@ from itertools import count
 # nx.draw(my_G, with_labels=1, node_color=desirability, cmap = cmap)
 # plt.show()
 
-def graph(adjacency_list):
+def graph(adjacency_list, des_map, current_name):
     my_G = nx.DiGraph()
 
-    for keys in adjacency_list:
-        #my_G.add_node(keys)
-        for each in adjacency_list[keys]:
-            my_G.add_edge(keys, each[1])
+    # for keys in adjacency_list:
+    #     #my_G.add_node(keys)
+    for each in adjacency_list[current_name]:
+        my_G.add_edge(current_name, each)
 
     k = my_G.nodes()
 
+    color_map = {}
+    for keys in des_map:
+        if des_map[keys]['value'] == 1.0:
+            color_map[keys] = 'green'
+        elif des_map[keys]['value'] == 0:
+            color_map[keys] = 'red'
+        else:
+            color_map[keys] = 'pink'
+
+    map_color = [color_map.get(node) for node in my_G.nodes()]
+
     my_G.graph
-    nx.draw(my_G, with_labels=1)
+    #nx.draw(my_G, with_labels=True, node_color = map_color, node_shape="s", node_size=1500)
+    nx.draw(my_G, with_labels=True, node_shape="s", node_color = map_color, node_size=1500, pos=nx.drawing.nx_agraph.graphviz_layout(
+        my_G,
+        prog='dot',
+        args='-Grankdir=LR'
+    ),
+    labels = {n: n.replace(";", "\n") for n in my_G.nodes})
+    print(my_G.number_of_nodes())
     plt.show()
--- main.py
+++ main.py
@@ -6,22 +6,31 @@ import copy
 import time
 
 from src.environment import Environment
-from src.planner import run_planning
+from src.planner import Planner
 from src.intention_recognition import Intention
+from src.desireability import CalculateDesireability
+from src.opportunity import OpportunityDetection
+from src.equilibrium_maintenance import Equilibrium_Maintenance
 
 from src.naive_proactivity import Naive
 
-from print_strategy import print_all
+from print_strategy import print_all, print_des, print_evolve_map
+from extract_graph import graph
 
 # My game start from here
 system = { }
 
 def setClasses():
-    env = Environment()
-    rec = Intention()
+    env = Environment(system)
+    pla = Planner()
+    rec = Intention(pla)
+    emq = Equilibrium_Maintenance(system)
+
 
     system["env"] = env
     system["recogniser"] = rec
+    system["emq"] = emq
+    system['pla'] = pla
 
     nav = Naive()
     system["nav"] = nav
@@ -30,114 +39,219 @@ def setClasses():
 def create_world_state(system):
 
     system['env'].add_type('main')
-    system['env'].add_sub_types('main', 'dish')
-    system['env'].add_sub_types('main', 'food')
+    system['env'].add_sub_types('main', 'obj')
+    system['env'].add_sub_types('main', 'agent')
+    system['env'].add_sub_types('main', 'weather')
+    system['env'].add_sub_types('main', 'time')
 
-    system['env'].add_action("Robot", "(?d - dish)", "(collected_all ?d)", "(submitted ?d)", "submit")
-    system['env'].add_action("Robot", "(?x - food)", "(not(collected ?x))", "(collected ?x)", "collect")
-    system['env'].add_action("Robot", "(?x - food)", "(collected ?x)", "(not(collected ?x))", "leave")
+    #adding agent's action for intention recognition
 
-    system['env'].add_predicate("collected_all ?d - dish")
-    system['env'].add_predicate("submitted ?d - dish")
-    system['env'].add_predicate("collected ?x - food")
-    system['env'].add_predicate("has_a ?s - dish ?x - food")
+    system['env'].add_action("Human", "(?u - agent)", "(not(outside ?u))", "(outside ?u)", "leave_home")
+    system['env'].add_action("Human", " ( ) ", "(dishes_dirty)", "(not(dishes_dirty))", "clean_dishes")
+    # system['env'].add_action("Human", "(?u - agent)", "(and (collected backpack) (collected compass) (outside ?u))", "(hiking ?u)", "go_hiking")
+    # system['env'].add_action("Human", "(?u - agent)", "(and (collected walking_stick) (collected dog) (outside ?u))", "(promenade ?u)", "go_promenade")
+    # system['env'].add_action("Human", "(?u - agent)", "(and (not (outside ?u)) )", "(watching_tv ?u)", "watch_tv")
+    # system['env'].add_action("Human", "(?u - agent)", "(and (not (outside ?u)) (collected book))", "(reading_book ?u)", "read_book")
+    system['env'].add_action("Human", "(?x - obj)", "(not(collected ?x))", "(collected ?x)", "collect")
+    system['env'].add_action("Human", "(?x - obj)", "(collected ?x)", "(not(collected ?x))", "leave")
+    #added robot actions for equilibrium maintenance, all of robot's action is communicative
 
-    system['env'].add_goal('( submitted soup )')
-    system['env'].add_goal('( submitted cake )')
-    system['env'].add_goal('( submitted smoothie )')
+    system['env'].add_action("Robot", "( )", "(dishes_dirty)", "(not(dishes_dirty))", "tell_clean_dishes")
+    system['env'].add_action("Robot", "(?x - obj)", "(not(collected ?x))", "(collected ?x)", "tell_gather")
+    system['env'].add_action("Robot", "(?u - agent)", "(current_weather hail)", "(not(outside ?u))", "warn_hail")
+#    system['env'].add_action("Robot", "( )", "(current_weather rain)", "(collected umbrella)", "warn_rain")
 
-    system['env'].add_object('dish')
-    system['env'].add_sub_objects('dish', 'soup')
-    system['env'].add_sub_objects('dish', 'cake')
-    system['env'].add_sub_objects('dish', 'smoothie')
-    system['env'].add_object('food')
-    system['env'].add_sub_objects('food', 'water')
-    system['env'].add_sub_objects('food', 'flour')
-    system['env'].add_sub_objects('food', 'lentil')
-    system['env'].add_sub_objects('food', 'chocolate')
-    system['env'].add_sub_objects('food', 'sugar')
-    system['env'].add_sub_objects('food', 'salt')
-    system['env'].add_sub_objects('food', 'pepper')
-    system['env'].add_sub_objects('food', 'milk')
-    system['env'].add_sub_objects('food', 'sprinkle')
-    system['env'].add_sub_objects('food', 'coco')
+    #added actions for free run, changes of concepts!
+    system['env'].add_action("Free", "(?wp - weather ?wn - weather)", "(current_weather ?wp)", "(and (not (current_weather ?wp)) (current_weather ?wn))", "weather_change")
+    system['env'].add_action("Free", "(?tp - time ?tn - time)", "(after ?tp ?tn)", "(and (not (current_time ?tp)) (current_time ?tn))", "time_change")
+    system['env'].add_action("Free", "(?u - agent)", "(breakfast ?u)", "(and  (not (breakfast ?u)) (dishes_dirty))", "had_breakfast")
+
+    system['env'].add_predicate("collected ?x - objects")
+    system['env'].add_predicate("outside ?u - agent")
+    system['env'].add_predicate("current_weather ?w - weather")
+    system['env'].add_predicate("current_time ?t - time")
+    system['env'].add_predicate("after ?t1 - time ?t2 - time")
+
+    system['env'].add_predicate("breakfast ?u - agent")
+
+    system['env'].add_predicate("weather_dealt")
+    system['env'].add_predicate("dishes_dirty")
+
+    #hiking
+    system['env'].add_goal('(and (collected backpack) (collected compass) (collected water_bottle) (outside user))')
+    #promenade
+    system['env'].add_goal('(and (collected walking_stick) (collected dog) (collected water_bottle) (outside user))')
+    #watching_tv
+    system['env'].add_goal('(and (not (outside user)) (collected water_bottle) (collected sugar) (collected tea) (collected milk))')
+    # Baking Cake
+    system['env'].add_goal('(and (not (outside user)) (collected sugar) (collected chocolate) (collected milk) (collected flour))')
+
+    system['env'].add_object('agent')
+    system['env'].add_sub_objects('agent', 'user')
+
+    system['env'].add_constants('weather')
+    system['env'].add_sub_constants('weather', 'sunshine')
+    system['env'].add_sub_constants('weather', 'hail')
+    system['env'].add_sub_constants('weather', 'rainy')
+    system['env'].add_sub_constants('weather', 'cloudy')
+    system['env'].add_constants('time')
+    system['env'].add_sub_constants('time', 'morning')
+    system['env'].add_sub_constants('time', 'noon')
+    system['env'].add_sub_constants('time', 'evening')
+    system['env'].add_constants('obj')
+#    system['env'].add_sub_constants('obj', 'hat')
+    system['env'].add_sub_constants('obj', 'compass')
+    system['env'].add_sub_constants('obj', 'backpack')
+    system['env'].add_sub_constants('obj', 'walking_stick')
+    system['env'].add_sub_constants('obj', 'dog')
+#    system['env'].add_sub_constants('obj', 'book')
+    system['env'].add_sub_constants('obj', 'water_bottle')
+#    system['env'].add_sub_constants('obj', 'umbrella')
+    system['env'].add_sub_constants('obj', 'tea')
+    system['env'].add_sub_constants('obj', 'milk')
+    system['env'].add_sub_constants('obj', 'chocolate')
+    system['env'].add_sub_constants('obj', 'flour')
+    system['env'].add_sub_constants('obj', 'sugar')
+
 
     #relationship added
-    system['env'].add_common_knowledge(" has_a soup water " )
-    system['env'].add_common_knowledge(" has_a soup salt " )
-    system['env'].add_common_knowledge(" has_a soup pepper " )
-    system['env'].add_common_knowledge(" has_a soup lentil " )
-    system['env'].add_common_knowledge(" has_a cake flour " )
-    system['env'].add_common_knowledge(" has_a cake chocolate " )
-    system['env'].add_common_knowledge(" has_a cake sugar " )
-    system['env'].add_common_knowledge(" has_a cake water " )
-    system['env'].add_common_knowledge(" has_a smoothie milk " )
-    system['env'].add_common_knowledge(" has_a smoothie water " )
-    system['env'].add_common_knowledge(" has_a smoothie sprinkle " )
-    system['env'].add_common_knowledge(" has_a smoothie coco " )
+    system['env'].add_common_knowledge(" after morning lunch " )
+    system['env'].add_common_knowledge(" after lunch after_noon " )
+    system['env'].add_common_knowledge(" after after_noon evening " )
+    system['env'].add_common_knowledge(" after evening night " )
+    system['env'].add_common_knowledge(" after night morning " )
 
-    #derived predicates_list
-    derived = " \n (:derived (collected_all ?s - dish) \n (forall (?x - food) \n (and \n (imply (has_a ?s ?x) (collected ?x)) \n (imply (and (not (has_a ?s ?x)) (collected ?x))  (not (collected ?x))) \n ) )  ) \n "
-    system['env'].add_derived(derived)
+    #related to the state description -> it could have chage later
+    system['env'].add_state_change("(current_weather sunshine)")
+    system['env'].add_state_change("(current_time morning)")
+    system['env'].add_state_change("(breakfast user)")
 
+    #ALSO add what is undesired situations to define which state will be undesired!
+    system['emq'].des.add_situation('get_wet', ['(current_weather rainy)' , '(outside ?u - agent)'], 0.7)
+    system['emq'].des.add_situation('get_hurt', ['(current_weather hail)' , '(outside ?u - agent)'], 1.0)
+    system['emq'].des.add_situation('dirt_dishes', ['(dishes_dirty)'], 0.4)
 
     domain_name, problem_name = system['env'].create_environment()
+
     return (domain_name, problem_name)
+
 
 def updateSituation(system):
     domain_name, problem_name = system['env'].create_environment()
     list_of_goals = system['env'].return_goal_list()
-    #intent + plan
-    intent = system['recogniser'].create_recogniser(list_of_goals, domain_name, problem_name)
-    print(intent)
 
-    desire = system['recogniser'].desirability_detection(intent, len(list_of_goals))
+    '''
+        HiR
+        Result of intention recognition
+        - intent = List of intent
+        - intent_map = Map of intent and plan to reach
+        - dynamic_k = length of plan
+    '''
+    intent, intent_map, dynamic_k = system['recogniser'].create_recogniser(list_of_goals, domain_name, problem_name)
 
-    plan_list = system['recogniser'].return_map()
-    evolve_map = system['env'].evolve_state(intent, plan_list, 8)
-    #evolve_map = system['env'].evolve_state([], plan_list, 3)
-    return plan_list
+
+    act_robot = system['env'].return_robot_action_list()
+    unvoluntary_action_list = system['env'].return_unvoluntary_action_list()
+    cur_state = system['env'].return_current_state()
+
+    defined_action = system['env'].create_action_list_map(unvoluntary_action_list)
+
+
+    '''
+        EQM
+
+        First create evolve_map - hypothetical map to forecast future
+    '''
+
+    #Define intention recogniton as opportunity
+    i = 0.2 #desirability value
+
+
+    ########################
+    # 1)
+    #Change K
+    K = 2 #look up strategy
+    #K = dynamic_k
+    #K = 0
+    ########################
+
+    react = time.time()
+    evolve_map = system['emq'].create_evolve_map(cur_state, defined_action, K)
+    react = time.time() - react
+
+    hashmap_state = system['emq'].return_state_hash_map()
+    #print_evolve_map(evolve_map)
+
+    cur_state_name = system['emq'].return_name_of_state(cur_state)
+
+    #############################
+    # 2)
+    #cut off brances
+    #evolve_map = system['emq'].des.cut_off_branches(evolve_map, hashmap_state, intent_map, defined_action, cur_state_name)
+
+    #limitate with K
+    #evolve_map = system['des'].limitate(evolve_map, hashmap_state, cur_state_name, dynamic_k)
+    print_evolve_map(evolve_map)
+    ###############################
+
+    #######################################
+    # 3 ) Changes desireability function
+    des = system['emq'].des.desirabilityFunction(evolve_map, hashmap_state)
+    print('______________________')
+    print('Desirabilily Calculation \n {}'.format(des))
+
+    #Change Desirability Value
+    #system['des'].update_desirability_Function(intent_map, i)
+
+    # des = system['des'].desirabilityFunction(evolve_map, hashmap_state)
+    # print('______________________')
+    # print('Desirabilily Calculation Modifed Intention\n {}'.format(des))
+    #############################################
+
+    #####
+    # 4)
+    #
+    #turn HiR actions into opportunity to match with EqM's oppotunities
+
+
+    oop_intent = system['emq'].oop.set_as_oop(intent_map, cur_state_name, cur_state, des, defined_action, i)
+    ###########
+
+
+    opp_eqm = system['emq'].oop.findOpportunity(evolve_map, des, cur_state_name, act_robot, K)
+
+    return opp_eqm, oop_intent, evolve_map, react, des
 
 
 if __name__ =='__main__':
     print("Hello World!")
     setClasses()
 
-    domain_name, problem_name = create_world_state(system)
+    '''
+       Please update the path name with your path name of fast_downward library
+    '''
+    #path = '~/Desktop/simulation_trial/DIRNAME'
+    system['pla'].set_path('~/planner/fast_downward/downward')
+    '''
+      Other search methods also could be use depend on the complexity of problem
+      Such as; "astar(lmcut())" , "astar(ipdb())" ...
+    '''
+    system['pla'].set_search_method("astar(add())")
 
+    domain_name, problem_name = create_world_state(system)
     #for every change in Situation
     react = time.time()
-    selected_plan = updateSituation(system)
+    opp_emq, opp_hir, state_evolvation, reaction_time, des = updateSituation(system)
     react = time.time() - react
 
-    print_all(react, system)
 
-    robot_said = system["nav"].select_action_to_play(selected_plan)
-    print(robot_said)
+    print_evolve_map(state_evolvation)
+    print('Final Map --------')
+    print_all(react, opp_emq, opp_hir, system)
 
+    cur_state = system['env'].return_current_state()
+    cur_state_name = system['emq'].return_name_of_state(cur_state)
 
-    #Situation change
-    print("action played -> collected pepper")
-    system['env'].add_state_change("collected pepper")
-
-    react = time.time()
-    selected_plan = updateSituation(system)
-    react = time.time() - react
-
-    print_all(react, system)
-
-    robot_said = system["nav"].select_action_to_play(selected_plan)
-    print(robot_said)
-
-    #Situation change
-    print("action played -> collected chocolate")
-    system['env'].add_state_change("collected chocolate")
-
-    react = time.time()
-    selected_plan = updateSituation(system)
-    react = time.time() - react
-
-    print_all(react, system)
-
-    robot_said = system["nav"].select_action_to_play(selected_plan)
-    print(robot_said)
+    print("Calculation Time -> {}".format(reaction_time))
+    print('Length = {}, {}'.format(len(state_evolvation[cur_state_name]),len(state_evolvation)))
+    graph(state_evolvation, copy.deepcopy(des), cur_state_name)
