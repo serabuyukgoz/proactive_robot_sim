1077b6594e3e72030ac7d4d75388ede4659753d0
Sera Buyukgoz <sera.buyukgoz@softbankrobotics.com>
2021-08-24 10:56:36 +0200

    better log of planner

--- src/planner.py
+++ src/planner.py
@@ -17,12 +17,24 @@ class Planner():
     def __init__(self):
         self.path = '~/planner/fast_downward/downward'
         self.search_method = "astar(add())"
+        self.python_version = "3"
 
     def set_path(self, path_of):
          self.path = path_of
 
+    def set_python_version(self, version):
+         self.python_version = version
+
     def set_search_method(self, method):
         self.search_method = method
+
+    def error_codes(self, code):
+
+        if (code == 30):
+            return ""
+        else:
+            error = str(code)
+            return error
 
     def run_planning(self, domain, problem):
       fd, tmp_path = tempfile.mkstemp()
@@ -30,15 +42,22 @@ class Planner():
 
       try:
           subprocess.check_call(
-              'python3 %s/fast-downward.py --plan-file %s %s %s --search "%s"'
-                  % (self.path, tmp_path, domain, problem, self.search_method),
+              'python%s %s/fast-downward.py --plan-file %s %s %s --search "%s"'
+                  % (self.python_version, self.path, tmp_path, domain, problem, self.search_method),
               stdout=f,
-              stderr=subprocess.STDOUT,
+              stderr=f,
               shell=True)
           with open(tmp_path, 'r') as tmp_file:
               a = tmp_file.read().partition(';')[0]
               fin = re.findall(r'\((.*?)\)', a)
               return fin
+
+      except subprocess.CalledProcessError as e:
+          error = self.error_codes(e.returncode)
+          logs = str()
+          with open("shell.txt", 'r') as logs:
+              a = logs.read()
+          raise Exception("Planner error log: %s \n Error code: %s" %(logs, error))
 
       finally:
           os.close(fd)
